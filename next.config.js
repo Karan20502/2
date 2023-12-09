3c23093726c5574436820d3150f1844e5b30948e
Karan20502 <153346499+Karan20502@users.noreply.github.com>
2023-12-09 12:42:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	async headers() {
 		return [
 			{
-				 source: "/api/(.*)",
+				 source: "/api/v1",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
